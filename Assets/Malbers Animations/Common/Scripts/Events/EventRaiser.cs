308339c8dfe5a75f3e164f02f831702988b95f97
AndyB8c <andy.bate@nomadgames.co.uk>
2021-01-15 11:23:09 +0000

    Update assets pt2

---
+++ Assets/Malbers Animations/Common/Scripts/Events/EventRaiser.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine; 
+namespace MalbersAnimations.Events
+{  
+    public class EventRaiser : MonoBehaviour
+    {
+        public UnityEngine.Events.UnityEvent onEnable;
+        public void OnEnable() => onEnable.Invoke();
+    }
+}
